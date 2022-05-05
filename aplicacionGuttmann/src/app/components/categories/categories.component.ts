8665a8b1762b4dbad32133dc76a83f9fc2b7a28e
Guillem <guillemsusbux@monlau.com>
2022-05-05 19:28:21 +0200

    estructura categorias hecha

--- aplicacionGuttmann/src/app/components/categories/categories.component.ts
+++ aplicacionGuttmann/src/app/components/categories/categories.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
+
 export class CategoriesComponent implements OnInit {
+  
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(){
+    $('button[category="category1"]').click(function (){
+      $('.div-game').hide();
+      $('div[category="category1"]').show();
+    }
+    )
 
-  ngOnInit(): void {
+    $('button[category="category2"]').click(function (){
+      $('.div-game').hide();
+      $('div[category="category2"]').show();
+    }
+    )
+
+    $('button[category="category3"]').click(function (){
+      $('.div-game').hide();
+      $('div[category="category3"]').show();
+    }
+    )
+
+    $('button[category="category4"]').click(function (){
+      $('.div-game').hide();
+      $('div[category="category4"]').show();
+    }
+    )
+
+    $('button[category="category5"]').click(function (){
+      $('.div-game').hide();
+      $('div[category="category5"]').show();
+    }
+    )
+
+    $('button[category="all"]').click(function (){
+      $('.div-game').show();
+    }
+    )
   }
 
 }
