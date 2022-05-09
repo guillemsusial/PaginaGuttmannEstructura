fa460702c8d2d0f44fbdcfdcf836593af37f6a3e
Raulmenmat <raulmenmat@monlau.com>
2022-05-09 19:21:14 +0200

    Game1
    9/5/22

--- aplicacionGuttmann/src/app/components/game/game.component.ts
+++ aplicacionGuttmann/src/app/components/game/game.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+ numbers:any;
+ game: Array<any> = [];
+ sequence:any;
 
-  constructor() { }
+  constructor() {
+    this.numbers = Array(5).fill(4).map((x,i)=>i); // [0,1,2,3,4]
+    
+   this.numbers = Array(12); // [4,4,4,4,4]
+   
+   this.ye()
+   
+  
+
+
+   //console.log(this.game);
+   }
+   
 
   ngOnInit(): void {
-  }
 
+    
+  }
+  
+  ye(){
+    setTimeout(() => {
+
+      for(let i=0;i<2;i++){     
+        this.game[i] = Math.ceil(Math.random() *12); // []
+        this.sequence= document.getElementById(this.game[i]); //
+      
+        this.sequence.classList.add("Active");
+        }
+    },200);
+
+    
+     
+    
+  }
 }
