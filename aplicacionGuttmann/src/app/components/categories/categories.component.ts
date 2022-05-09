383eb4d3ebd9961fa65c2dc74899c9e74930773d
Guillem <guillemsusbux@monlau.com>
2022-05-09 16:26:26 +0200

    Update categories.component.ts

--- aplicacionGuttmann/src/app/components/categories/categories.component.ts
+++ aplicacionGuttmann/src/app/components/categories/categories.component.ts
@@ -14,37 +14,32 @@ export class CategoriesComponent implements OnInit {
 
   ngOnInit(){
     $('.div-game').show();
-    
+
     $('button[category="category1"]').click(function (){
       $('.div-game').hide();
       $('div[category="category1"]').show();
     }
     )
-
     $('button[category="category2"]').click(function (){
       $('.div-game').hide();
       $('div[category="category2"]').show();
     }
     )
-
     $('button[category="category3"]').click(function (){
       $('.div-game').hide();
       $('div[category="category3"]').show();
     }
     )
-
     $('button[category="category4"]').click(function (){
       $('.div-game').hide();
       $('div[category="category4"]').show();
     }
     )
-
     $('button[category="category5"]').click(function (){
       $('.div-game').hide();
       $('div[category="category5"]').show();
     }
     )
-
     $('button[category="all"]').click(function (){
       $('.div-game').show();
     }
