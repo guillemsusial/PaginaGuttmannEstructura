f6c6b4bdc626c92303127e6550ffd32ae1c5da92
Guillem <guillemsusbux@monlau.com>
2022-05-10 18:24:59 +0200

    user_10_5_graficos

--- aplicacionGuttmann/src/app/components/user/user.component.ts
+++ aplicacionGuttmann/src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CargarScriptsService } from 'src/app/cargar-scripts.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _CargarScripts:CargarScriptsService) { 
+
+    _CargarScripts.Carga(["graficoLinea"]);
+    _CargarScripts.Carga(["graficoHex"]);
+  }
 
   ngOnInit(): void {
+    
+    
   }
 
 }
