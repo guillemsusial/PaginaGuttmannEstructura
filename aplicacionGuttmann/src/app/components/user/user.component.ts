d164a4e7c9abe37a460c205f677a97a362217675
Guillem <guillemsusbux@monlau.com>
2022-05-11 17:12:01 +0200

    panelUserActualizado_11_05

--- aplicacionGuttmann/src/app/components/user/user.component.ts
+++ aplicacionGuttmann/src/app/components/user/user.component.ts
@@ -10,30 +10,16 @@ declare var $: any;
 })
 export class UserComponent implements OnInit {
 
-  constructor(private _CargarScripts:CargarScriptsService) { 
+  constructor(private _CargarScripts: CargarScriptsService) {
 
     _CargarScripts.Carga(["graficoLinea"]);
     _CargarScripts.Carga(["graficoPolar"]);
   }
 
   ngOnInit(): void {
-    $('button[category="lineChart"]').hide();
-    $('div[category="polarChart"]').hide();
-    $('button[category="lineChart"]').addClass("button-categories-active");
-    $('button[category="polarChart"]').addClass("button-categories-active");
-
-    $('button[category="lineChart"]').click(function (){
-      $('button[category="lineChart"]').hide();
-      $('div[category="polarChart"]').fadeOut("fast");
-      $('div[category="lineChart"]').fadeIn("fast");
-      $('button[category="polarChart"]').show();
-    }
-    )
-    $('button[category="polarChart"]').click(function (){
-      $('button[category="polarChart"]').hide();
-      $('div[category="lineChart"]').fadeOut("fast");
-      $('button[category="lineChart"]').fadeIn("fast");
-      $('div[category="polarChart"]').show();
+    $('button[category="lineChart"]').click(function lineChart() {
+      $('div[category="polarChart"]').toggle();
+      $('div[category="lineChart"]').toggle();
     }
     )
   }
