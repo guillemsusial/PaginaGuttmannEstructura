5f22a1253ef23b720253f06a0003bd2095fa5cec
Marc <marcsegbla@campus.monlau.com>
2022-05-18 19:28:52 +0200

    loginTerminado18/05

--- aplicacionGuttmann/src/app/app-routing.module.ts
+++ aplicacionGuttmann/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TemplateComponent } from './components/template/template.component';
 import { UserComponent } from './components/user/user.component';
 import { WelcomeCompComponent } from './components/welcome-comp/welcome-comp.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'home', component: WelcomeCompComponent},
-  { path: 'game', component: TemplateComponent},
-  { path: 'user', component: UserComponent},
-  { path: '**', redirectTo: 'home'}
+  { path: 'home', component: WelcomeCompComponent },
+  { path: 'game', component: TemplateComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
