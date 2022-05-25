a50d19942cb3cdde656eb3910c5fd004c2ac631b
Marc <marcsegbla@campus.monlau.com>
2022-05-25 17:54:08 +0200

    cambio

--- aplicacionGuttmann/src/app/app-routing.module.ts
+++ aplicacionGuttmann/src/app/app-routing.module.ts
@@ -10,19 +10,18 @@ import { TemplateComponent } from './components/template/template.component';
 import { TutorialComponent } from './components/tutorial/tutorial.component';
 import { UserComponent } from './components/user/user.component';
 import { WelcomeCompComponent } from './components/welcome-comp/welcome-comp.component';
-import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
 
-  { path: 'home', component: WelcomeCompComponent},
-  { path: 'game', component: TemplateComponent},
-  { path: 'user', component: UserComponent},
+  { path: 'home', component: WelcomeCompComponent },
+  { path: 'game', component: TemplateComponent },
+  { path: 'user', component: UserComponent },
 
-  { path: 'simon', component:GameComponent},
-  { path: 'sequence', component:SequenceComponent},
-  { path: 'login', component: LoginComponent},
+  { path: 'simon', component: GameComponent },
+  { path: 'sequence', component: SequenceComponent },
+  { path: 'login', component: LoginComponent },
 
-  { path: '**', redirectTo: 'home'}
+  { path: '**', redirectTo: 'home' }
 
 ];
 
