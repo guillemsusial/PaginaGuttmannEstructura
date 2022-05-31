6f6ac54997f1414f95a591465feac7345bd5cdde
Guillem <guillemsusbux@monlau.com>
2022-05-31 16:50:02 +0200

    Comentariospalnene

--- aplicacionGuttmann/src/app/components/header/header.component.ts
+++ aplicacionGuttmann/src/app/components/header/header.component.ts
@@ -38,14 +38,22 @@ export class HeaderComponent implements OnInit {
     this.crudService.logout();
   }
 
+  //FUNCIÓN PARA ENVIAR LOS DATOS DEL LOGIN
   enviarDatos(): any {
+    //LLAMADA AL SERVICIO CRUD CON LOS DATOS DEL LOGIN
     this.crudService.LoginUser(this.userForm.value).subscribe((data) => {
+      //RECOGEMOS LOS DATOS DEL USUARIO ENCRIPTADOS QUE NOS DEVUELVE EL LOGIN Y LO CONVERTIMOS A JSON
       this.jsonObject = JSON.parse(data);
+      //SI EL MENSAJE ES "success" HACEMOS LO SIGUENTE:
       if (this.jsonObject.message == "success") {
+        //GUARDAMOS EL TOKEN EN LOCAL CON EL SERVICIO DE CRUD
         this.crudService.saveToken(this.jsonObject.token);
+        //REDIRIGIMOS AL USUARIO A LA PAGINA PRINCIPAL
         this.router.navigateByUrl('');
+        //LE DECIMOS A LA VARIABLE loggedIn QUE ES true
         this.crudService.loggedIn.next(true);
       } else {
+        //SI NO ES CORRECTO EL LOGIN LE DECIMOS QUE LA VARIABLE loggedIn ES false
         this.crudService.loggedIn.next(false);
       }
     }, (error) => {
