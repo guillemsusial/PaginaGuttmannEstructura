cca0882a5c9920774b705e3b6b70b1bb374925dd
Guillem <guillemsusbux@monlau.com>
2022-06-03 17:09:13 +0200

    inicioMetodoSerieLuces

--- aplicacionGuttmann/src/app/components/tutorial/tutorial.component.ts
+++ aplicacionGuttmann/src/app/components/tutorial/tutorial.component.ts
@@ -1,53 +1,18 @@
-import { Component,  OnInit, Input } from '@angular/core';
-import { } from "src/app/components/categories/categories.component";
+import { Component, OnInit, Input } from '@angular/core';
+import {} from 'src/app/components/categories/categories.component';
 import { ActivatedRoute, Params } from '@angular/router';
-
 
 @Component({
   selector: 'app-tutorial',
   templateUrl: './tutorial.component.html',
   styleUrls: ['./tutorial.component.css'],
-
-
-
-
-
 })
-
 export class TutorialComponent implements OnInit {
-   /* el_juego="";
-    juegos=['juego1',''];
-    descripcion="";
-    descripciones=['Observa la secuencia','2','3'];
+  game: { game: string };
 
-
-    @Input() idd="0";
-
-    //Datos recibidos del juego seleccionado
-    juegorecibido='';*/
-    game: {game: string
-
-    };
-
-
-
-
-  constructor(private rutaActiva: ActivatedRoute) { 
-
-
-    this.game={game:this.rutaActiva.snapshot.params['game']};
+  constructor(private rutaActiva: ActivatedRoute) {
+    this.game = { game: this.rutaActiva.snapshot.params['game'] };
   }
 
-  ngOnInit(): void {
-    //this.cargarJuego(0);
-
-  }
-/*
-  cargarJuego(id:number){
-
-    this.el_juego=this.juegos[id];
-    this.descripcion=this.descripciones[id]
-
-  }*/
-
+  ngOnInit(): void {}
 }
